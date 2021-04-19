46e7e80c6db3b3f19cd1197b715bd73c9fb5a657
Victor <vbaziuk13@gmail.com>
2021-04-19 15:37:26 -0400

    update link

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/maps/place/232+S+Goodman+St,+Rochester,+NY+14607/@43.1489839,-77.5907407,3a,75y,98.63h,90t/data=!3m6!1e1!3m4!1syHv-iM1u7lJuObm6rTG0hg!2e0!7i16384!8i8192!4m5!3m4!1s0x89d6b50be18fb4a3:0x1f9c0d9b2783ca3c!8m2!3d43.1489426!4d-77.5904314"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Take me home
         </a>
       </header>
     </div>
